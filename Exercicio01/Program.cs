d986315c532cda9e0e1aab2470c2fd9451d31ba5
andrejava2022 <andre.java.ads.2022@gmail.com>
2024-01-12 20:30:16 -0300

    Exercicio 01 - resolvido

--- Exercicio01/Program.cs
+++ Exercicio01/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+ *  Escreva um programa que leia um caminho de arquivo do usuário e, em seguida, verifique se o arquivo 
+    existe. Se o arquivo existir, mostre a data de criação do arquivo, caso contrário, exiba uma mensagem 
+    informando que o arquivo não foi encontrado.
+
+ */
+
+Console.Write("Digite o caminho do arquivo: ");
+string caminhoDoArquivo = Console.ReadLine();
+
+try
+{
+    FileInfo fileInfo = new FileInfo(caminhoDoArquivo);
+    if (fileInfo.Exists)
+    {
+        Console.Write($"\nData de criação do arquivo: {fileInfo.CreationTime.ToString("dd/MM/yyyy HH:mm" )}");
+    }
+    else
+    {
+        Console.WriteLine($"\nO arquivo {caminhoDoArquivo} não foi encontrado");
+    }
+}
+catch(Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
+
+
+Console.ReadKey();
