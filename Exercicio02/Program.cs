8a7d03641d18d7dcab268883347272af35c76d28
andrejava2022 <andre.java.ads.2022@gmail.com>
2024-01-12 21:03:35 -0300

    Exercicio 02 - resolvido

---
+++ Exercicio02/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+    Escreva um programa que copie um arquivo de um diretório para outro. O nome e o caminho do arquivo de 
+    origem e destino devem ser fornecidos pelo usuário. 
+*/
+
+Console.Write("Digite o nome do arquivo que deseja mover: ");
+string arquivo = Console.ReadLine();
+
+Console.Write("\nDigite o caminho de origem do arquivo: ");
+string caminhoDeOrigem = Console.ReadLine();
+
+Console.Write("\nDigite o caminho de destino do arquivo: ");
+string caminhoDeDestino = Console.ReadLine();
+
+caminhoDeOrigem = caminhoDeOrigem + "/" + arquivo;
+caminhoDeDestino = caminhoDeDestino + "/" + arquivo;
+
+try
+{
+    File.Move(caminhoDeOrigem, caminhoDeDestino);
+    Console.WriteLine($"Arquivo movido de {caminhoDeOrigem} para {caminhoDeDestino}");
+}
+catch(Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
+Console.ReadKey();
