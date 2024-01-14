9897ebd9e1f7c0946b03b2c35895c1127ee7fd65
andrejava2022 <andre.java.ads.2022@gmail.com>
2024-01-14 13:06:51 -0300

    Exercicio 08 - finalizado

---
+++ Exercicio08/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+    8- Criar um programa que permita ao usuário criar um novo diretório e, em seguida, criar um arquivo dentro desse diretório usando a classe Directory e a classe Path. 
+*/
+
+try
+{
+    Console.Write("Digite o caminho para o novo diretório: ");
+    string caminho = Console.ReadLine();
+
+    Console.Write("Digite o nome do novo diretório: ");
+    string nome = Console.ReadLine();
+
+    // Obtendo o caminho completo para o novo diretório
+    string caminhoCompleto = Path.Combine(caminho, nome);
+
+    // Criando o diretório
+    Directory.CreateDirectory(caminhoCompleto);
+    Console.WriteLine($"Diretório criado com sucesso em: {caminhoCompleto}\n");
+
+    Console.Write("Digite o nome do arquivo que deseja criar: ");
+    string nomeArquivo = Console.ReadLine();
+    
+    // Criando arquivo no novo diretório
+    File.Create(Path.Combine(caminhoCompleto, nomeArquivo + ".txt"));
+    Console.WriteLine($"Arquivo criado com sucesso em: {Path.Combine(caminhoCompleto, nomeArquivo)}");
+}
+catch(Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
+Console.ReadKey();
