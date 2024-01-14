41fb4741c2cbf3f16e2283105e9b517687529107
andrejava2022 <andre.java.ads.2022@gmail.com>
2024-01-13 21:51:56 -0300

    Exercicio 07 - resolvido

---
+++ Exercicio07/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+7- Criar um programa que liste todos os subdiretórios em um determinado diretório usando a classe DirectoryInfo. 
+*/
+Console.Write("Digite o caminho do diretório que deseja listar: ");
+string diretorio = Console.ReadLine();
+Console.WriteLine();
+
+if (diretorio != null)
+{
+    try
+    {
+        DirectoryInfo dirInfo = new DirectoryInfo(diretorio);
+        foreach (var subdiretorio in dirInfo.GetDirectories())
+        {
+            Console.WriteLine(subdiretorio.Name);
+        }
+    }
+    catch(Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+}
+
+Console.ReadKey();
